21cd176f368aff19a2780859a61f9e961905acba
CristianCerpa <ccerpaj@gmail.com>
2025-09-10 10:22:28 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      backgroundColor: '#000',
+      color: '#fff',
+      fontSize: '2rem'
+    }}>
+      <p>Hola, Next.js en Vercel 🚀</p>
+    </div>
+  );
+}
