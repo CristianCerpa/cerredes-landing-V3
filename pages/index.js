e97d06ee58b8d96ecabcaf678da167afb6236084
CristianCerpa <ccerpaj@gmail.com>
2025-09-13 21:10:02 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ export default function Home() {
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
-          padding: "2rem 2rem", // margen del logo a la izquierda
+          padding: "1rem 2rem", // margen del logo a la izquierda
           zIndex: 10,
           background: "#2c2c2c",
         }}
