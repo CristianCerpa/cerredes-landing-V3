609d9d6f5258b058c826e3bf0cc2c4ba5b33ac63
CristianCerpa <ccerpaj@gmail.com>
2025-09-24 21:48:56 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function Home() {
       >
         {/* Logo */}
         <div>
-          <Image src="/logo.png" alt="Logo" width={150} height={100} />
+          <Image src="/logo.png" alt="Logo" width={100} height={100} />
         </div>
 
         {/* Menú + carrito */}
